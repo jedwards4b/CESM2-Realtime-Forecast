70f30ff196f4c1a4e19b3aa6d9bbce080ae585e2
Jim Edwards <jedwards@ucar.edu>
2019-11-22 07:19:36 -0700

    rsync done in st_archive script

--- bin/lndboundary.sh
+++ bin/lndboundary.sh
@@ -1,12 +1,13 @@
-#!/usr/bin/env bash
+#!/usr/bin/env bash 
 source ~/.bash_profile
 cd CESM2-Realtime-Forecast
 ./bin/getCDASdata.py
-./bin/create_landforcing_from_NCEPCFC.ncl 
+export CYLC_TASK_CYCLE_POINT=`date +%Y-%m-%d -d yesterday`
+ncl ./bin/create_landforcing_from_NCEPCFC.ncl
+
 ./bin/update_land_streams.py --case/glade/scratch/ssfcst/I2000Clm50BgcCrop.002runContd/
 cd /glade/scratch/ssfcst/I2000Clm50BgcCrop.002runContd/
 ./xmlchange STOP_N=1
 ./xmlchange STOP_OPTION=ndays
 ./case.submit
 
-ssh data-access rsync -zavh --size-only /glade/scratch/ssfcst/archive/I2000Clm50BgcCrop.002runContd/rest/ /glade/campaign/cesm/development/cross-wg/S2S/land/rest/
