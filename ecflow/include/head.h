6fee3f31a6ee2ed3cbe78ae6beced2513ac71c1b
James Edwards <jedwards@ucar.edu>
2023-11-03 09:10:41 -0600

    update header

--- ecflow/include/head.h
+++ ecflow/include/head.h
@@ -4,8 +4,8 @@ set -x          # echo script lines as they are executed
 set -o pipefail # fail if last(rightmost) command exits with a non-zero status
  
 # Defines the variables that are needed for any communication with ECF
-export ECF_PORT=%ECF_PORT%    # The server port number
-export ECF_HOST=%ECF_HOST%    # The host name where the server is running
+export ECF_PORT=${ECF_PORT:=%ECF_PORT%}    # The server port number
+export ECF_HOST=${ECF_HOST:=%ECF_HOST%}    # The host name where the server is running
 export ECF_NAME=%ECF_NAME%    # The name of this current task
 export ECF_PASS=%ECF_PASS%    # A unique password, used for job validation & zombie detection
 export ECF_TRYNO=%ECF_TRYNO%  # Current try number of the task
@@ -14,7 +14,7 @@ export FCST_HOME=%FCST_HOME%
 export FCST_WORK=%FCST_WORK%
 export CESM_ROOT=%CESM_ROOT%
 export CESM_WORKFLOW=%CESM_WORKFLOW%
-  
+module use %FCST_HOME%/modulefiles  
 # export NO_ECF=1             # uncomment to run as a standalone task on the command line
 # Optionally define the path where to find ecflow_client
 # make sure client and server use the *same* version.
